3bd82bd9abfa294f1d24737fc3aaa99ed6ac9c65
ivantrj <ivantrajanovski@gmail.com>
2020-11-26 15:04:09 +0100

    Initial Commit

---
+++ IsolatedWordRecognitionVolleyball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IsolatedWordRecognitionVolleyball
+//
+//  Created by Ivan Trajanovski on 2020. 11. 26..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
